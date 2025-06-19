1f79a8373424ad6dd416175c8ad603a5baeedb90
savnimaheshwari <143203323+savnimaheshwari@users.noreply.github.com>
2025-06-19 19:45:44 -0400

    Update Intro.js

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,7 +3,6 @@ import React from "react";
 import "../styles/Intro.css";
 import Typist from "react-typist";
 import "react-typist/dist/Typist.css";
-import EmailRoundedIcon from "@material-ui/icons/EmailRounded";
 import FadeInSection from "./FadeInSection";
 import FractalTree from "./FractalTree";
 
@@ -25,7 +24,7 @@ class Intro extends React.Component {
   render() {
     return (
       <div id="intro">
-        <FractalTree></FractalTree>
+        <FractalTree />
         <Typist avgTypingDelay={120}>
           <span className="intro-title">
             {"hi, "}
@@ -33,28 +32,7 @@ class Intro extends React.Component {
             {" here."}
           </span>
         </Typist>
-        <FadeInSection>
-          <div className="intro-subtitle">I code and write stuff sometimes.</div>
-          <div className="intro-desc">
-            My name is Savni Maheshwari, and I am an undergraduate student at{' '}
-            <a href="https://www.purdue.edu/">Purdue University</a>, pursuing a
-            Bachelor's degree in {' '} <a href="https://www.cs.purdue.edu/">Data Science</a>, offered by the CS dept. along with a certificate in
-            Entrepreneurship & Innovation.
-            <p>
-              I am currently seeking internships for Summer 2025 in roles related
-              to machine learning and data science. With a passion for leveraging
-              data-driven insights, I am eager to contribute to innovative
-              projects and grow my expertise in these fields.
-            </p>
-          </div>
-          <a
-            href="mailto:maheshwarisavni@gmail.com"
-            className="intro-contact"
-          >
-            <EmailRoundedIcon></EmailRoundedIcon>
-            {" Get in touch !"}
-          </a>
-        </FadeInSection>
+        {/* Removed intro subtitle and get in touch */}
       </div>
     );
   }
