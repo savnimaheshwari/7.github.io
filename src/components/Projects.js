1fbb3739f123cbd28651210390ccc1176483609a
savnimaheshwari <143203323+savnimaheshwari@users.noreply.github.com>
2025-06-18 20:27:37 -0400

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -67,39 +67,46 @@ class Projects extends React.Component {
           "Prepared a dataset from 2013-2023 on electric vehicle registrations and associated state-level variables. Analyzed the relationships using supervised machine learning methods. Presented the results of the analysis.",
         techStack: "R, Machine Learning, Python",
         open: "https://docs.google.com/presentation/d/1F9UZ1B06Vppn5-6rvqEe9CUf33BZT47qTisyD0mCP7Q/edit?usp=sharing",
-        link: "https://github.com/savnimaheshwari/EV-Research---IE-4900"
+        link: "https://github.com/savnimaheshwari/EV-Research---IE-4900",
+        image: "/assets/purdue-ie.png"
       },
       "Social Media/Gym Post Sentiment Analysis (PHIL 208)": {
         desc:
           "My team and I addressed the pervasive influence of social media, particularly by fitness influencers. We conducted sentiment analysis on a vast dataset of gym-related content across various social media and developed a website for 20,000+ college students to use.",
         techStack: "Data collection , Data analysis",
         open:
-          "https://drive.google.com/file/d/1AULR6kgrWeMQUMp8wkosE4K0TxQ58bcl/view?usp=sharing"
+          "https://drive.google.com/file/d/1AULR6kgrWeMQUMp8wkosE4K0TxQ58bcl/view?usp=sharing",
+        image: "/assets/gym-sentiment.png"
       },
       "Bookstore management software": {
         desc:
           "For my IBDP Computer Science Internal Assessment, I developed a Bookstore Management Software. The project streamlined inventory, sales, and customer management processes. It featured functionalities like tracking stock levels, generating invoices, and maintaining a customer database, all built to improve efficiency and enhance user experience for bookstore operations.",
-        techStack: "Java, MySQL"
+        techStack: "Java, MySQL",
+        image: "/assets/bookstore.png"
       },
       "Kathak: Visharad": {
         desc:
           "I obtained the Visharad Pratham certificate in Kathak after 8 years, equivalent to a Bachelor's in dance. I’ve taught Kathak to kids as part of a social program and participated in stage shows.",
         techStack: "Dancing (practical + theory)",
-        open: "https://drive.google.com/file/d/1qVCiW7Px6HWCAcE_6w9Dc86xqo5EVIN_/view?usp=sharing"
+        open: "https://drive.google.com/file/d/1qVCiW7Px6HWCAcE_6w9Dc86xqo5EVIN_/view?usp=sharing",
+        image: "/assets/kathak.png"
       },
 
       "Project on AI-Based Switching Mechanisms for Wireless Charging Systems for EVs": {
         desc:
           "Working with Professor Tiwari, ex-Dean of IIT Kharagpur, I focused on applying AI to optimize configurations and architecture in wireless charging systems.",
         techStack: "MATLAB",
-        open: "https://drive.google.com/file/d/1CO-Lj5h8z93VLv5lgXaYjy264_hgZbKM/view?usp=sharing"
+        open: "https://drive.google.com/file/d/1CO-Lj5h8z93VLv5lgXaYjy264_hgZbKM/view?usp=sharing",
+        image: "/assets/ai-charging.png"
       },
        "Static Test Pad for Rocket Motors (Avionics Intern@ STAR)": {
         desc:
           "My team and I at STAR Labs, Gujurat built a static test pad for high-powered rockets using software like Proteus (avionics bay) and SolidWorks (mechanical design) enhancing reliability by 20%.",
         techStack: "Proteus, SolidWorks",
         open:
-          "https://drive.google.com/file/d/1_PwD1YMJ-s3djLAGFhWM5I4kUoq55qGw/view?usp=sharing"}
+          "https://drive.google.com/file/d/1_PwD1YMJ-s3djLAGFhWM5I4kUoq55qGw/view?usp=sharing",
+        image: "/assets/rocket-testpad.png"
+      }
     };
 
     return (
@@ -147,6 +154,16 @@ class Projects extends React.Component {
                       openLink={projects[key].open}
                     />
                   </div>
+
+                  {/* Small project image */}
+                  {projects[key].image && (
+                    <img
+                      src={projects[key].image}
+                      alt={`${key} preview`}
+                      className="project-small-image"
+                    />
+                  )}
+
                   <div className="card-title">{key}</div>
                   <div className="card-desc">{projects[key]["desc"]}</div>
                   <div className="card-tech">{projects[key]["techStack"]}</div>
